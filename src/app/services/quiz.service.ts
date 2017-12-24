218fa16d27068325ebef1fbca48206214da82b57
Ranga Bhupal Reddy Gangavarapu <RGangavarapu@manh.com>
2017-12-24 21:39:51 +0530

    quiz service

--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
+import { Question } from "../models/question";
 
 @Injectable()
 export class QuizService {
@@ -14,8 +15,38 @@ export class QuizService {
 
   getAll() {
     return [
-  	  { id: 'data/aws.json', name: 'AWS' }
+      { id: 'data/aws1.json', name: 'AWS' },
+      { id: 'http://localhost:3000/aws', name: 'AWS Live' },
+      { id: 'data/aspnet.json', name: 'aspnet' }
     ];
+  }
+
+  save(question: Question) {
+    this.http.post('http://localhost:3000/questions', question).subscribe((response) => {
+      let question = response.json();
+      let qid = question.id;
+      let ansCount: number = 0;
+
+      question.options.forEach((element, index) => {
+        element.questionId = qid;
+        element.id = 10000 + (qid * 10) + index;
+        if (element.isAnswer)
+          ansCount++;
+      });
+
+      if (ansCount > 0) {
+        if (ansCount === 1)
+          question.questionTypeId = 1;
+        else if (ansCount > 1)
+          question.questionTypeId = 2;
+      }
+
+      this.http.put('http://localhost:3000/questions/'+qid, question).subscribe((response) => {
+        console.log(response.json());
+      });
+    });
+
+    return true;
   }
 
 }
